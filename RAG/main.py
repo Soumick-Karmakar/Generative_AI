2b599baa5b72340247434a6889b487769337dbc8
Soumick-Karmakar <soumick.karmakar1998@gmail.com>
2026-02-13 10:01:54 +0530

    langchain set up

--- RAG/main.py
+++ RAG/main.py
@@ -16,6 +16,17 @@ RAG will be implemented using LangChain.
 So to initialize the LangChain, we need to run the command 'uv init' in the terminal. 
 This will create a 'uv.lock' file in the project directory, 
 which is used to manage dependencies and ensure that the correct versions of packages are installed.
+
+Basic steps to set up the environment for RAG using LangChain:
+1. uv init
+2. uv venv
+3. source .venv/Scripts/activate
+4. Create a 'requirements.txt' file and add the following dependencies:
+    - langchain
+    - langchain-core
+    - langchain-community
+5. uv add -r requirements.txt
+
 '''
 
 def main():
